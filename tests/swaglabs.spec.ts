7aa5334fcc1d6c85204bb29ad39c86d717cb26a4
loneswingset <ievmorkunaite@gmail.com>
2024-09-14 19:16:55 +0200

    clarified variables

--- tests/swaglabs.spec.ts
+++ tests/swaglabs.spec.ts
@@ -1,22 +1,29 @@
 import { test, expect } from '@playwright/test';
 
+const validUser = 'standard_user';
+const validPassword = 'secret_sauce';
+
 test('login with valid credentials', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
   await page.locator('[data-test="username"]').click();
-  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="username"]').fill(validUser);
   await page.locator('[data-test="password"]').click();
-  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="password"]').fill(validPassword);
   await page.locator('[data-test="login-button"]').click();
+
+  // checks
   await expect(page.locator('[data-test="title"]')).toHaveText('Products');
 });
 
 test('login with invalid credentials', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
   await page.locator('[data-test="username"]').click();
-  await page.locator('[data-test="username"]').fill('asdf');
+  await page.locator('[data-test="username"]').fill('invalidUser');
   await page.locator('[data-test="password"]').click();
-  await page.locator('[data-test="password"]').fill('asdf');
+  await page.locator('[data-test="password"]').fill('invalidPassword');
   await page.locator('[data-test="login-button"]').click();
+
+  // checks
   await expect(page.locator('[data-test="error"]')).toHaveText(
     'Epic sadface: Username and password do not match any user in this service'
   );
@@ -25,14 +32,16 @@ test('login with invalid credentials', async ({ page }) => {
 test.beforeEach(async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
   await page.locator('[data-test="username"]').click();
-  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="username"]').fill(validUser);
   await page.locator('[data-test="password"]').click();
-  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="password"]').fill(validPassword);
   await page.locator('[data-test="login-button"]').click();
 });
 
 test('Add to cart button changes to Remove', async ({ page }) => {
   await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+
+  // checks
   await expect(
     page.locator('[data-test="remove-sauce-labs-backpack"]')
   ).toBeVisible();
@@ -41,6 +50,8 @@ test('Add to cart button changes to Remove', async ({ page }) => {
 test('test', async ({ page }) => {
   await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
   await page.locator('[data-test="shopping-cart-link"]').click();
+
+  // checks
   await expect(page.locator('[data-test="inventory-item"]')).toBeVisible();
 });
 
@@ -48,5 +59,7 @@ test('removing item from cart on the cart page', async ({ page }) => {
   await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
   await page.locator('[data-test="shopping-cart-link"]').click();
   await page.locator('[data-test="remove-sauce-labs-backpack"]').click();
+
+  // checks
   await expect(page.locator('[data-test="inventory-item"]')).not.toBeVisible();
 });
