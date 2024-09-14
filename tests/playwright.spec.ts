01a5261c33456fb08eaf45716165a00963003853
loneswingset <ievmorkunaite@gmail.com>
2024-09-14 19:37:14 +0200

    merge example with playwright

--- tests/playwright.spec.ts
+++ tests/playwright.spec.ts
@@ -35,3 +35,23 @@ test('generating tests', async ({ page }) => {
     page.getByRole('heading', { name: 'Generating tests' })
   ).toBeVisible();
 });
+
+// TESTS FROM EXAMPLE
+test('has title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(
+    page.getByRole('heading', { name: 'Installation' })
+  ).toBeVisible();
+});
